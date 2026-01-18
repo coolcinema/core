8a7a45198de698e8cbcfc88ba7380044388a6276
mr-scrpt <48452376+mr-scrpt@users.noreply.github.com>
2026-01-18 12:31:18 +0000

    chore(release): update registry code and version [skip ci]

--- packages/registry/src/index.ts
+++ packages/registry/src/index.ts
@@ -13,8 +13,8 @@ export type ServiceName = typeof ServiceNames[keyof typeof ServiceNames];
 export const Registry = {
   Identity: {
     name: ServiceNames.Identity,
-    port: 3000,
-    url: 'http://identity-service.coolcinema.svc.cluster.local:3000',
+    port: 5000,
+    url: 'http://identity-service.coolcinema.svc.cluster.local:5000',
     description: 'User authentication and profile management'
   },
   Sales: {
